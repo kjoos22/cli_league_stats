57a56e40f528fd27cc5ef7203181dedc15ae8c1d
kjoos22 <kjoos22@gmail.com>
2020-10-25 14:54:22 -0400

    prompt user for summoner name and account for spaces

--- lib/api.rb
+++ lib/api.rb
@@ -1,10 +1,18 @@
 class API
 
-    def self.retrieve_summoner
+    def self.retrieve_summoner(summoner)
         url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name"+
         "/Lord%20Juicifer?api_key=#{ENV['API_KEY']}"
-        uri = URI(url)
-        response = Net::HTTP.get(uri)
+        
+        response = JSON.parse(Net::HTTP.get(URI(url)))
+        accountID = response['accountId']
+
+        url = "https://na1.api.riotgames.com/lol/match/v4/matchlists/"+
+        "by-account/wK2VyEwzVGbwEtT16PHHcvsrgQdKDJ1BprwyfgAKm6yA2Q?api_key="+
+        "#{ENV['API_KEY']}"
+        
+        
+        
     end
 
 end
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,8 +1,12 @@
 class CLI
 
     def start
-        puts "Welcome to CLI_League_Stats"
-        API.retrieve_summoner
+        puts "Welcome to CLI_League_Stats!"
+        print "Please enter a summoner name: "
+        summoner = gets()
+        summoner = summoner.gsub(' ', '%20')
+        puts summoner
+        API.retrieve_summoner(summoner)
     end
 
 end
