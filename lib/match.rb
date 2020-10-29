8fb267d7b3603ab398f5c17e9bf975289a38eb49
kjoos22 <kjoos22@gmail.com>
2020-10-29 18:12:22 -0400

    matches now sorted by queue

--- lib/match.rb
+++ lib/match.rb
@@ -1,11 +1,19 @@
 class Match
-    attr_accessor :matchid, :champion_played, :result, :queue, :date_time
+    attr_accessor :matchid, :champion_played, :result, :queue, :date_time,
+                  :summoner
 
-    def initialize(matchid, champion_played, queue, date_time)
+    def initialize(summoner, matchid, champion_played, queue, date_time)
+        @summoner = summoner
         @matchid = matchid
         @champion_played = champion_played
-        @queue = queue
+        @queue = $queue_id[queue]
         @date_time = Time.at(0, date_time, :millisecond)
+    end
+
+    def sort_match(summoner)
+
+              
+            
     end
 
 end
--- lib/summoner.rb
+++ lib/summoner.rb
@@ -1,19 +1,29 @@
 class Summoner
-    attr_accessor :name, :matches
+    attr_accessor :name, :matches, :sorted_matches
 
     def initialize(name, match_history)
         @name = name
         @matches = []
+        @sorted_matches = {}
         match_history['matches'].each do |match|          
-            @matches << Match.new(match['gameId'], match['champion'],
+            @matches << Match.new(self, match['gameId'], match['champion'],
                                  match['queue'], match['timestamp'])       
         end
         @matches = @matches.sort_by {|match| match.date_time}.reverse
-        binding.pry
+        sort_matches
     end
 
     def sort_matches
-        #@matches.each do 
+        @matches.each do |match|
+            if !@sorted_matches[match.queue]
+                @sorted_matches[match.queue] = []
+                @sorted_matches[match.queue] << match
+            else
+                @sorted_matches[match.queue] << match
+            end
+        end
+        @matches = []
+        binding.pry
     end
 
 end
